5aaf50945244b7274669c32305158f0479514153
Brett Needham <brettneedham88@gmail.com>
2020-11-17 16:50:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,6 @@ programmer_hash[:yukihiro_matsumoto] = {
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
